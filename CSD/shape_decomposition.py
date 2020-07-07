db1d60c1d6ab0ef18e8c9cac59db467d67f4e9c0
aliabd <29164686+aAbdz@users.noreply.github.com>
2020-07-07 10:26:36 +0300

    modify param

--- CSD/shape_decomposition.py
+++ CSD/shape_decomposition.py
@@ -523,30 +523,30 @@ def object_analysis(obj, skel):
         junction_coordinates = s['dec_nodes']
         sub_skeleton = s['skeleton']
         
-        dec_obj = obj.copy()
+        rec_obj = obj.copy()
         for junction_coordinate in junction_coordinates:
             
-            st_cross_sections, interpolated_skel, sub_skeleton = junction_correction(dec_obj, sub_skeleton, junction_coordinate, 
+            st_cross_sections, interpolated_skel, sub_skeleton = junction_correction(rec_obj, sub_skeleton, junction_coordinate, 
                                                                    g_radius=15, g_res=0.25, H_th=0.7, shift_impose=1, Euler_step_size=0.5)
             
-            if len(interpolated_skel) <= 10: continue
             
             interpolated_skel = np.array(interpolated_skel)               
             filled_cs = filling_cross_sections(st_cross_sections, g_radius=15, g_res=0.25)                
-            dec_obj = object_decomposition(dec_obj, interpolated_skel, filled_cs, g_radius=15, g_res=0.25)
+            rec_obj = object_decomposition(rec_obj, interpolated_skel, filled_cs, g_radius=15, g_res=0.25)
         
-            labeled_obj = label(dec_obj, neighbors=4)
+            labeled_obj = label(rec_obj, neighbors=4)
             for region in regionprops(labeled_obj):
-                if region.area>=len(sub_skeleton):
-                    
-                    obj = np.zeros(labeled_obj.shape, dtype=np.bool)
+                if region.area>=len(sub_skeleton):                    
+                    dec_obj = np.zeros(labeled_obj.shape, dtype=np.bool)
                     for coordinates in region.coords:
-                        obj[coordinates[0], coordinates[1], coordinates[2]] = True
+                        dec_obj[coordinates[0], coordinates[1], coordinates[2]] = True
                     
-                    if detect_main_obj(obj, sub_skeleton):  
-                        decomposed_objs.append(region.coords)
+                    if detect_main_obj(dec_obj, sub_skeleton):  
+                        decomposed_objs.append(dec_obj)
                         decomposed_skeletons.append(sub_skeleton)
+                                                
                         break
+                    
                     
     return decomposed_objs, decomposed_skeletons
     
